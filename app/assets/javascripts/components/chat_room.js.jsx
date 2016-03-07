22172644fef4bdd2665803a0c9a5a11241fc4545
Nicole <nicole.j.paz@gmail.com>
2016-03-06 18:25:18 -0800

    Move channel code to react component. Update component state instead of appending partial.

--- app/assets/javascripts/components/chat_room.js.jsx
+++ app/assets/javascripts/components/chat_room.js.jsx
@@ -1,8 +1,37 @@
 var ChatRoom = React.createClass({
+  getInitialState: function() {
+    var messages = this.props.messages || [];
+    return {messages: messages};
+  },
+
+  updateMessageList: function(message) {
+    var messages = this.state.messages;
+    messages.push(message);
+    this.setState({messages: messages});
+  },
+
+  componentDidMount: function() {
+    App.room = App.cable.subscriptions.create("RoomChannel", {
+      connected: function() {},
+
+      disconnected: function() {},
+
+      received: function(data) {
+        this.updateMessageList(data.message);
+      },
+
+      speak: function(message) {
+        return this.perform('speak', { message: message });
+      },
+
+      updateMessageList: this.updateMessageList
+    });
+  },
+
   render: function() {
     return (
       <div>
-        <MessageList messages={this.props.messages} />
+        <MessageList messages={this.state.messages} />
         <MessageForm />
       </div>
     );
